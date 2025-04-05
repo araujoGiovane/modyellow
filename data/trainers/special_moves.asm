8a01c0e0c77ad413d25d0219ccab2cef03ab614b
Giovane <araujofgiovane@gmail.com>
2025-04-05 20:35:42 -0300

    todos os niveis foram reduzidos, falta verificar os moves

--- data/trainers/special_moves.asm
+++ data/trainers/special_moves.asm
@@ -1,5 +1,5 @@
 INCLUDE "data/trainers/special_moves_1.inc"
 INCLUDE "data/trainers/special_moves_2.inc"
 INCLUDE "data/trainers/special_moves_3.inc"
-INCLUDE "data/trainers/special_moves_4.inc"
+
 
