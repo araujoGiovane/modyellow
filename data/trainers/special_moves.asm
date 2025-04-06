3042d55be99290b9f743c7e9fc11176951ee4d24
Giovane <araujofgiovane@gmail.com>
2025-04-06 14:37:47 -0300

    todos os movimentos corrigidos e revisados

--- data/trainers/special_moves.asm
+++ data/trainers/special_moves.asm
@@ -1,5 +1,7 @@
-INCLUDE "data/trainers/special_moves_1.inc"
-INCLUDE "data/trainers/special_moves_2.inc"
-INCLUDE "data/trainers/special_moves_3.inc"
+INCLUDE "data/trainers/moves_1_gym.inc"
+INCLUDE "data/trainers/moves_2_elite.inc"
+INCLUDE "data/trainers/moves_3_rival1.inc"
+INCLUDE "data/trainers/moves_4_rival2.inc"
+INCLUDE "data/trainers/moves_5_champ.inc"
 
 
