b8d043e9c3af021e21167c242bd08cb27c88c1b2
Giovane <araujofgiovane@gmail.com>
2025-04-07 21:01:24 -0300

    alterações na floresta de viridian

--- data/wild/maps/ViridianForest.asm
+++ data/wild/maps/ViridianForest.asm
@@ -1,13 +1,13 @@
 ViridianForestWildMons:
 	def_grass_wildmons 25 ; encounter rate
+	db  3, WEEDLE
+	db  5, WEEDLE
 	db  3, CATERPIE
-	db  4, METAPOD
-	db  4, CATERPIE
 	db  5, CATERPIE
 	db  4, PIDGEY
 	db  6, PIDGEY
-	db  6, CATERPIE
-	db  6, METAPOD
+	db  7, METAPOD
+	db  7, KAKUNA
 	db  8, PIDGEY
 	db  9, PIDGEOTTO
 	end_grass_wildmons
