35ea27313a9e7168a51b02ff9c56499816bcfdef
Giovane <araujofgiovane@gmail.com>
2025-04-10 20:11:04 -0300

    atualização 7 feita

--- data/wild/maps/VictoryRoad3F.asm
+++ data/wild/maps/VictoryRoad3F.asm
@@ -1,10 +1,10 @@
 VictoryRoad3FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 36, GEODUDE
+	db 36, GRAVELER
 	db 44, GOLBAT
-	db 41, GEODUDE
+	db 41, GRAVELER
 	db 49, ONIX
-	db 46, GEODUDE
+	db 46, GRAVELER
 	db 41, GRAVELER
 	db 42, MACHOKE
 	db 45, MACHOKE
